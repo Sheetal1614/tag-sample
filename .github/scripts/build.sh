0e3d766c1c1df21d9dc6654be31fa7d28f7f6ae2
Sheetal1614 <sehrawats1614@gmail.com>
2022-11-23 14:00:50 +0530

    Add refactored code

--- .github/scripts/build.sh
+++ .github/scripts/build.sh
@@ -1,33 +1,33 @@
 #!/bin/bash
-version="latest_int"
+version="latest"
 if [ $(git tag -l "$version") ]; then
-  version="second_latest_int"
+  version="second_latest"
     if [ $(git tag -l "$version") ]; then
-      version="third_latest_int"
+      version="third_latest"
       if [ $(git tag -l "$version") ]; then
-        git tag -d third_latest_int
-        git push origin :third_latest_int
+        git tag -d third_latest
+        git push origin :third_latest
       fi
-      x=$(git rev-parse second_latest_int)
-      git tag -d second_latest_int
-      git push origin :second_latest_int
-      git tag third_latest_int ${x}
-      y=$(git rev-parse latest_int)
-      git tag -d latest_int
-      git push origin :latest_int
-      git tag second_latest_int ${y}
-      git tag latest_int HEAD
+      x=$(git rev-parse second_latest)
+      git tag -d second_latest
+      git push origin :second_latest
+      git tag third_latest ${x}
+      y=$(git rev-parse latest)
+      git tag -d latest
+      git push origin :latest
+      git tag second_latest ${y}
+      git tag latest HEAD
       git push --tags
     else
-      z=$(git rev-parse latest_int)
-      git tag -d latest_int
-      git push origin :latest_int
-      git tag second_latest_int ${z}
-      git tag latest_int HEAD
-      git push  origin latest_int second_latest_int
+      z=$(git rev-parse latest)
+      git tag -d latest
+      git push origin :latest
+      git tag second_latest ${z}
+      git tag latest HEAD
+      git push  origin latest_int second_latest
     fi
 else
-    git tag latest_int HEAD
-    git push origin latest_int
+    git tag latest HEAD
+    git push origin latest
 fi
 
