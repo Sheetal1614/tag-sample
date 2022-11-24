297caf36a8ff285f0d74156080cdb2befebab2b9
Sheetal1614 <sehrawats1614@gmail.com>
2022-11-24 11:27:46 +0530

    Update code:script

--- .github/scripts/build.sh
+++ .github/scripts/build.sh
@@ -1,34 +1,60 @@
 #!/bin/bash
-version="latest"
-if [ $(git tag -l "$version") ]; then
-  version="second_latest"
-    if [ $(git tag -l "$version") ]; then
-      version="third_latest"
-      if [ $(git tag -l "$version") ]; then
+if [ $(git tag -l "third_latest") ]; then
         git tag -d third_latest
         git push origin :third_latest
-      fi
+fi
+if [ $(git tag -l "second_latest") ]; then
       x=$(git rev-parse second_latest)
       git tag -d second_latest
       git push origin :second_latest
       git tag third_latest ${x}
-      y=$(git rev-parse latest)
-      git tag -d latest
-      git push origin :latest
-      git tag second_latest ${y}
-      git tag latest HEAD
-      git push --tags
-    else
+      git push  origin third_latest
+fi
+if [ $(git tag -l "latest") ]; then
       z=$(git rev-parse latest)
       git tag -d latest
       git push origin :latest
       git tag second_latest ${z}
-      git tag latest HEAD
-      git push  origin latest_int second_latest
-    fi
-else
-    git tag latest HEAD
-    git push origin latest
 fi
+git tag latest HEAD
+git push origin latest
 
-#this is sample comment
+#
+#version="latest"
+#if [ $(git tag -l "$version") ]; then
+#  version="second_latest"
+#    if [ $(git tag -l "$version") ]; then
+#      version="third_latest"
+#      if [ $(git tag -l "$version") ]; then
+#        git tag -d third_latest
+#        git push origin :third_latest
+#      fi
+#      x=$(git rev-parse second_latest)
+#      git tag -d second_latest
+#      git push origin :second_latest
+#      git tag third_latest ${x}
+#      y=$(git rev-parse latest)
+#      git tag -d latest
+#      git push origin :latest
+#      git tag second_latest ${y}
+#      git tag latest HEAD
+#      git push --tags
+#    else
+#      z=$(git rev-parse latest)
+#      git tag -d latest
+#      git push origin :latest
+#      git tag second_latest ${z}
+#      git tag latest HEAD
+#      git push  origin latest_int second_latest
+#    fi
+#else
+#    git tag latest HEAD
+#    git push origin latest
+#fi
+
+
+#
+#def abc(v1, v2, v3)
+#
+#end
+#abc(1,2,3)
