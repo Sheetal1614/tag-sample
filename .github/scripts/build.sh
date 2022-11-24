568c152552d2ba7a7ad8a56c21b784107e030ea4
Sheetal1614 <sehrawats1614@gmail.com>
2022-11-24 12:33:58 +0530

    add fourth commit

--- .github/scripts/build.sh
+++ .github/scripts/build.sh
@@ -20,38 +20,6 @@ fi
 git tag latest HEAD
 git push origin latest
 
-#
-#version="latest"
-#if [ $(git tag -l "$version") ]; then
-#  version="second_latest"
-#    if [ $(git tag -l "$version") ]; then
-#      version="third_latest"
-#      if [ $(git tag -l "$version") ]; then
-#        git tag -d third_latest
-#        git push origin :third_latest
-#      fi
-#      x=$(git rev-parse second_latest)
-#      git tag -d second_latest
-#      git push origin :second_latest
-#      git tag third_latest ${x}
-#      y=$(git rev-parse latest)
-#      git tag -d latest
-#      git push origin :latest
-#      git tag second_latest ${y}
-#      git tag latest HEAD
-#      git push --tags
-#    else
-#      z=$(git rev-parse latest)
-#      git tag -d latest
-#      git push origin :latest
-#      git tag second_latest ${z}
-#      git tag latest HEAD
-#      git push  origin latest_int second_latest
-#    fi
-#else
-#    git tag latest HEAD
-#    git push origin latest
-#fi
 
 
 #
